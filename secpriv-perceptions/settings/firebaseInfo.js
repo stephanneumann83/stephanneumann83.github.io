4b7db1828759cba1db0109ce20f67d881cde5f05
okskulyk <okskulyk@gmail.com>
2020-12-21 13:54:57 +0100

    adding firebase connection + updated statements

--- secpriv-perceptions/settings/firebaseInfo.js
+++ secpriv-perceptions/settings/firebaseInfo.js
@@ -1,10 +1,13 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyDRo-tdOk_wniBWQ",
-    authDomain: "html-q-demo.firebaseapp.com",
-    databaseURL: "https://html-q-demo.firebaseio.com",
-    storageBucket: "html-q-demo.appspot.com",
-    messagingSenderId: "558977"
-};
-firebase.initializeApp(config);
-var rootRef = firebase.database().ref();
+  apiKey: "AIzaSyAhzHjQLgvyAuodHMM97lgcCjE_cJZCj1U",
+  authDomain: "security-perceptions.firebaseapp.com",
+  databaseURL: "https://security-perceptions-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "security-perceptions",
+  storageBucket: "security-perceptions.appspot.com",
+  messagingSenderId: "1053287696702",
+  appId: "1:1053287696702:web:4cca82f9ceda3f1c5a046f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  var rootRef = firebase.database().ref();
